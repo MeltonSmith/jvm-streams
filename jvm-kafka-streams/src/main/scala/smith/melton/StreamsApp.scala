589b4bc81d7738be8897f13cd8bce7b9025a0e9c
MeltonSmith <ravenglc1@gmail.com>
2025-06-03 12:15:54 +0500

    agg

--- jvm-kafka-streams/src/main/scala/smith/melton/StreamsApp.scala
+++ jvm-kafka-streams/src/main/scala/smith/melton/StreamsApp.scala
@@ -2,15 +2,16 @@ package smith.melton
 
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.kafka.common.utils.Exit
-import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
-import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.scala.kstream.{Consumed, KStream}
+import org.apache.kafka.streams.kstream.{JoinWindows, Named}
+import org.apache.kafka.streams.scala.{ByteArrayKeyValueStore, StreamsBuilder}
+import org.apache.kafka.streams.scala.kstream.{Consumed, Grouped, KStream, Materialized, Produced}
 import org.apache.kafka.streams.scala.serialization.Serdes._
+import org.apache.kafka.streams.state.{KeyValueStore, QueryableStoreTypes, Stores}
+import org.apache.kafka.streams.{AutoOffsetReset, KafkaStreams, StreamsConfig}
 import smith.melton.model.MoneyTransfer
 import smith.melton.serde.JsonSerde._
 
 import java.util
-import java.util.Properties
 
 /**
  * @author Melton Smith
@@ -31,11 +32,27 @@ object StreamsApp extends App {
 
   private val builder = new StreamsBuilder()
 
-  private val stream: KStream[String, MoneyTransfer] = builder.stream("transfers_topic")(Consumed.`with`[String, MoneyTransfer])
-    .peek((a,b) => println(b))
+  builder.stream("transfers_topic")(Consumed.`with`[String, MoneyTransfer]
+                                                .withOffsetResetPolicy(AutoOffsetReset.earliest()))
+    .groupBy((_, v) => v.toUserId)(Grouped.`with`[Long, MoneyTransfer])
+    .aggregate(0L)((_,v,c) => {
+       c + v.long
+//    })((Materialized.as[Long, Long](Stores.inMemoryKeyValueStore("in mem"))))
+    })(Materialized.`with`[Long, Long, ByteArrayKeyValueStore])
+    .toStream(Named.as("default_store"))
+    .peek((k, v) => {
+      println(s"key $k, value $v")
+      println(s"================")
+    })
+//    .count()(Materialized.as[String, Long](Stores.inMemoryKeyValueStore("in mem")))
+    .to("output-1")(Produced.`with`[Long, Long])
 
 
-  private val streams = new KafkaStreams(builder.build(),new StreamsConfig(mapFromSet))
+
+  private val topology = builder.build()
+  println(topology.describe())
+
+  private val streams = new KafkaStreams(topology,new StreamsConfig(mapFromSet))
   streams.cleanUp()
   streams.start()
 
